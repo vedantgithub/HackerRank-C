3e0823e22de5aa6c6ef941b59d856f81ada2101d
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-06 12:11:37 +0530

    Create DigitFrequency.c

---
+++ DigitFrequency.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+     char s[1000];
+int cnt=0,i,j;
+
+scanf("%s", s);
+for(j=0;j<10;j++)
+{
+    for (i=0;s[i]!='\0';i++)
+    {
+        if(s[i]-48==j)
+        cnt++;
+    }
+    printf("%d " ,cnt);
+    cnt=0;
+
+
+}
+
+return 0;
+}
