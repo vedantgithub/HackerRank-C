ffa7f5c8ca6c36af36bd1985f53de64cd8d20e41
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-15 00:23:45 +0530

    Create DataTypes.java

---
+++ 30DaysCode/DataTypes.java
@@ -0,0 +1,25 @@
+  
+       
+
+        /* Declare second integer, double, and String variables. */
+    int a;
+    double b;
+    String inputstring ; 
+     
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        a = scan.nextInt();
+        b = scan.nextDouble();
+        scan.nextLine();
+        inputstring = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+System.out.println(i+a);
+
+        /* Print the sum of the double variables on a new line. */
+		
+System.out.println(d + b);
+        /* Concatenated print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+System.out.println(s+inputstring);
