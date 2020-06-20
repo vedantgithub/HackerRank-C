8a2fcb60dd05abab3c5f35fac4caed3b37b5b4ee
vedantgithub <49688373+vedantgithub@users.noreply.github.com>
2020-06-20 20:56:41 +0530

    Create Reverse a linked list.java

---
+++ Data Structure/Linked-List/Reverse a linked list.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    // Complete the reverse function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode reverse(SinglyLinkedListNode head) {
+                 if( head == null)
+                 return head;
+                 SinglyLinkedListNode prev = null;
+                 SinglyLinkedListNode current_node = head;
+
+                 while(current_node != null)
+                 {
+                     SinglyLinkedListNode next_node = current_node.next;
+                     current_node.next = prev;
+                     prev = current_node;
+                     current_node = next_node;
+                 }
+                 return prev;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
